2bd32609467a863dddf53caa48f9bbd419bc3c0d
ahomu <mail@ayumusato.com>
2012-10-16 10:38:29 +0900

    Add setup suite scripts

---
+++ initialize.sh
@@ -0,0 +1,25 @@
+cd ~/
+
+# brew
+ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
+
+# rvm
+curl -L https://get.rvm.io | bash -s stable --ruby
+
+# nvm
+git clone git://github.com/creationix/nvm.git ~/.nvm
+
+# from easy_install
+sudo easy_install Pygments
+sudo easy_install http://closure-linter.googlecode.com/files/closure_linter-latest.tar.gz
+
+# from Homebrew
+brew install ack
+brew install emacs
+brew install tmux
+brew install git
+brew install wget
+brew install tree
+brew install pstree
+brew install phantomjs
+brew install closure-compiler
---
+++ symlinks.sh
@@ -0,0 +1,13 @@
+CURRENT=`pwd`
+USERDIR=`echo ~/`
+
+echo install from $CURRENT to $USERDIR
+
+ln -s $CURRENT/ackrc ~/.ackrc
+ln -s $CURRENT/zshrc ~/.zshrc
+ln -s $CURRENT/zsh ~/.zsh
+ln -s $CURRENT/tmux.conf ~/.tmux.conf
+ln -s $CURRENT/gitconfig ~/.gitconfig
+ln -s $CURRENT/gitignore ~/.gitignore
+ln -s $CURRENT/emacs.d ~/.emacs.d
+#ln -s $CURRENT/ssh_config ~/.ssh/config
