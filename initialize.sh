5aa29d7a272a814f13e792b46646e09ea4c753d7
ahomu <mail@ayumusato.com>
2014-01-16 11:19:39 +0900

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,26 +1,36 @@
 cd ~/
 
 # brew
-ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
 # rvm
 curl -L https://get.rvm.io | bash -s stable --ruby
 
 # nvm
-git clone git://github.com/creationix/nvm.git ~/.nvm
-
-# from easy_install
-sudo easy_install Pygments
-sudo easy_install http://closure-linter.googlecode.com/files/closure_linter-latest.tar.gz
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
 # from Homebrew
 brew install ack
 brew install emacs
 brew install tmux
 brew install git
+brew install hub
 brew install wget
 brew install tree
 brew install pstree
 brew install phantomjs
+brew install casperjs
 brew install closure-compiler
 brew install imagesnap
+brew install python
+
+# from gem
+gem install tmuxinator
+gem install chef
+gem install knife-solo
+gem install berkshelf
+
+# from pip
+pip install --upgrade distribute
+pip install pygments
+pup install ansible
